95817e76bc27302b0a147a1896b595584002d1d6
Sergio Xalambrí <hello@sergiodxa.com>
2019-06-24 11:59:01 -0500

    Add PWA (#1)
    * Register Offline First Service Worker
    
    * Save Data Offline
    
    * Complete manifest.json

--- src/store.js
+++ src/store.js
@@ -2,9 +2,23 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import offlineSync from "./middlewares/offline-sync";
 
 const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composer(applyMiddleware(thunk, logger)));
+if (localStorage.getItem("version") !== process.env.REACT_APP_VERSION) {
+  localStorage.removeItem("state");
+}
+
+const initialState = { todos: {}, filter: "NONE", sortBy: "DUE_DATE_DESC" };
+const preloadedState = JSON.parse(
+  localStorage.getItem("state") || JSON.stringify(initialState)
+);
+
+const store = createStore(
+  reducer,
+  preloadedState,
+  composer(applyMiddleware(thunk, offlineSync, logger))
+);
 
 export default store;
